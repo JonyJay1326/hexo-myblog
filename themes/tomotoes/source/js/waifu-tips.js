e56656ce90a329cbd31ffcc4813a9f8416196293
FlyingAir <827199132@qq.com>
2019-01-30 09:37:31 +0800

    change api

--- themes/tomotoes/source/js/waifu-tips.js
+++ themes/tomotoes/source/js/waifu-tips.js
@@ -219,7 +219,7 @@ function loadModel(modelId, modelTexturesId){
     localStorage.setItem('modelId', modelId);
     if (modelTexturesId === undefined) modelTexturesId = 0;
     localStorage.setItem('modelTexturesId', modelTexturesId);
-    loadlive2d('live2d', '//api.fghrsh.net/live2d/get/?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
+    loadlive2d('live2d', '//api3.fghrsh.net/live2d/get/?id='+modelId+'-'+modelTexturesId, console.log('live2d','模型 '+modelId+'-'+modelTexturesId+' 加载完成'));
 }
 
 function loadRandModel(){
@@ -230,7 +230,7 @@ function loadRandModel(){
 
     $.ajax({
         cache: false,
-        url: '//api.fghrsh.net/live2d/'+modelTexturesRandMode+'_textures/?id='+modelId+'-'+modelTexturesId,
+        url: '//api3.fghrsh.net/live2d/'+modelTexturesRandMode+'_textures/?id='+modelId+'-'+modelTexturesId,
         dataType: "json",
         success: function (result){
             if (result.textures['id'] == 1 && (modelTexturesId == 1 || modelTexturesId == 0)) {
@@ -250,7 +250,7 @@ function loadOtherModel(){
 
     $.ajax({
         cache: false,
-        url: '//api.fghrsh.net/live2d/'+modelTexturesRandMode+'/?id='+modelId,
+        url: '//api3.fghrsh.net/live2d/'+modelTexturesRandMode+'/?id='+modelId,
         dataType: "json",
         success: function (result){
             loadModel(result.model['id']);
